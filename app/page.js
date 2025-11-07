c3e11ea278ce218e56cb7a820dc5cc9058a4766f
Francesco Merighi <francescomerighi61@gmail.com>
2025-11-07 17:58:49 +0100

    Insert logo

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-dvh h-[100vw]">
-      <h1 className="text-4xl">Hello World!</h1>
+    <main className="flex items-center justify-center h-dvh w-screen">
+      <Image className="w-96 h-auto" src="/econverto-logo.svg" alt="ECONVERTO | Logo" width={175} height={20} />
     </main>
   );
 }
